7b66731b562fb1db693271de1bbda7a1685fce9a
NadiaM <134788321+Nadya-Maksimova@users.noreply.github.com>
2023-05-27 16:59:51 +0200

    Update date.js

--- date.js
+++ date.js
@@ -2,7 +2,7 @@
 const date = new Date()
 const getDate = function(date) {
     let readableDate = date.toLocaleString()
-    return readableDate.slice(0, 9)
+    return readableDate.slice(0, 10)
   }
 
 const dateAll = document.querySelectorAll(".date")
